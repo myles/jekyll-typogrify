f8f39a8da4f7dfee8b70829ff0c67adaa5f4ef64
Myles Braithwaite <me@mylesbraithwaite.com>
2015-04-11 00:08:42 -0400

    Testing the testing.

--- spec/jekyll/typogrify_spec.rb
+++ spec/jekyll/typogrify_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
-describe Jekyll::Typogrify do
+describe(Jekyll) do
+  let(:overrides) do
+    {
+      "source"      => source_dir,
+      "destination" => dest_dir,
+      "url"         => "http://example.org",
+    }
+  end
+  
+  let(:config) do
+    Jekyll.configuration(overrides)
+  end
+  
+  let(:site)      { Jekyll::Site.new(config) }
+  let(:contents)  { File.read(dest_dir("index.html")) }
+  
+  before(:each) do
+    site.process
+  end
+  
   it 'has a version number' do
     expect(Jekyll::Typogrify::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
