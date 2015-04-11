e70fbc35c042c0e13cb16b3b1453d10cf68799ef
Myles Braithwaite <me@mylesbraithwaite.com>
2015-04-11 00:15:37 -0400

    Trying to fix all the stuff.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,22 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'jekyll/typogrify'
+require 'jekyll'
+
+Jekyll.logger.log_level = :error
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.order = 'random'
+
+  SOURCE_DIR = File.expand_path("../fixtures", __FILE__)
+  DEST_DIR   = File.expand_path("../dest",     __FILE__)
+
+  def source_dir(*files)
+    File.join(SOURCE_DIR, *files)
+  end
+
+  def dest_dir(*files)
+    File.join(DEST_DIR, *files)
+  end
+end
